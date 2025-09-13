dbf5e18cedabdb46a6a31a9ab683a2e3ef4ffe3b
EgorSad06 <143409348+EgorSad06@users.noreply.github.com>
2025-09-13 23:00:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -453,7 +453,7 @@ document.addEventListener('DOMContentLoaded', function() {
             tempContainer.appendChild(clonedSvg);
 
             // Устанавливаем фиксированные размеры для clonedSvg, соответствующие viewBox
-            let svgWidth = 1600; // Из index.html
+            let svgWidth = 1300; // Из index.html
             let svgHeight = 1000; // Из index.html
 
             // Копируем viewBox с оригинального SVG, это критично для корректного отображения
